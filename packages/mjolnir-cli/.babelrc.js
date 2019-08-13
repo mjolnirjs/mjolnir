6483149cabdf9813e80d177ce463ae4bac0d139d
Leo Hui <leohxj@gmail.com>
2019-08-13 23:28:12 +0800

    feat: use babel-preset to build mjolnir-cli

---
+++ packages/mjolnir-cli/.babelrc.js
@@ -0,0 +1,12 @@
+// .babelrc can merge with babel.config.js
+module.exports = {
+  presets: [
+    [
+      '@mjolnir/babel-preset',
+      {
+        react: false,
+        typescript: true
+      }
+    ]
+  ]
+};

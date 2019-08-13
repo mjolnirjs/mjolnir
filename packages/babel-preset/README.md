6fe11897494ecaba6bbe1729cb95e334d7b496de
Leo Hui <leohxj@gmail.com>
2019-08-13 17:49:03 +0800

    feat: add deps

--- packages/babel-preset/README.md
+++ packages/babel-preset/README.md
@@ -1,4 +1,8 @@
 # @mjolnir/babel-preset
+A babel preset for es/ts/react project with stage-2.
+
+## Introduction
+
 
 ## install
 `yarn add @babel/core @mjolnir/babel-preset -D`
--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,39 +1,69 @@
 'use strict';
 
 const { declare } = require('@babel/helper-plugin-utils');
+const env = process.env.NODE_ENV;
 
-module.exports = declare((api, { react, typescript }) => {
+module.exports = declare((api, opts) => {
   api.assertVersion(7);
 
+  const { react, typescript, modules = false } = opts;
+
+  if (!env) {
+    throw new Error(`require specify 'NODE_ENV' environment variables.`);
+  }
   if (typeof react !== 'boolean') {
-    throw new Error('.react must be a boolean, or undefined');
+    throw new Error("options 'react' must be a boolean, or undefined");
   }
   if (typeof typescript !== 'boolean') {
-    throw new Error('.typescript must be a boolean, or undefined');
+    throw new Error("option 'typescript' must be a boolean, or undefined");
   }
+
+  // @babel/plugin-proposal-decorators should comes before @babel/plugin-proposal-class-properties.
+  // When using the legacy: true option of @babel/plugin-proposal-decorators, @babel/plugin-proposal-class-properties must be used in loose: true mode.
   const plugins = [
-    require.resolve('@babel/plugin-transform-runtime'),
-    [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-    [
-      require.resolve('@babel/plugin-proposal-class-properties'),
-      { loose: true }
-    ],
-    require.resolve('@babel/plugin-proposal-object-rest-spread'),
-    require.resolve('@babel/plugin-syntax-dynamic-import')
+    // Stage 2
+    [require('@babel/plugin-proposal-decorators'), { legacy: true }],
+    require('@babel/plugin-proposal-function-sent'),
+    require('@babel/plugin-proposal-export-namespace-from'),
+    require('@babel/plugin-proposal-numeric-separator'),
+    require('@babel/plugin-proposal-throw-expressions'),
+    // Stage 3
+    require('@babel/plugin-syntax-dynamic-import'),
+    require('@babel/plugin-syntax-import-meta'),
+    [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+    require('@babel/plugin-proposal-json-strings')
   ];
 
-  const presets = [];
+  const presets = [
+    // default preset is @babel/preset-env
+    [
+      require('@babel/preset-env'),
+      {
+        //TODO: how about other opts
+        modules // default to false, webpack takes care of modules
+      }
+    ]
+  ];
 
-  if (typescript) presets.push(require.resolve('@babel/preset-typescript'));
+  if (typescript) presets.push(require('@babel/preset-typescript'));
 
-  presets.push([
-    require.resolve('@babel/preset-env'),
-    {
-      modules: false
+  if (react) {
+    presets.push([
+      require('@babel/preset-react'),
+      {
+        development: env
+      }
+    ]);
+    if (env === 'production') {
+      // Remove PropTypes from production build
+      plugins.push([
+        require('babel-plugin-transform-react-remove-prop-types'),
+        {
+          removeImport: true
+        }
+      ]);
     }
-  ]);
-
-  if (react) presets.push(require.resolve('@babel/preset-react'));
+  }
 
   return {
     presets,

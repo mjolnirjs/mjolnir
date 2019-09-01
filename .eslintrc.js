d4e458e926b5f065cc773ddef9234b83ab9bf015
Leo Hui <leohxj@gmail.com>
2019-09-01 09:53:32 +0800

    feat: update prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 const { babel, typescript } = require('@mjolnir/eslint-config/overrides');
 
 module.exports = {
-  "extends": ["@mjolnir"],
-  "overrides": [
-    babel,
-    typescript
-  ],
-  "rules": {},
-}
+  extends: ['@mjolnir'],
+  overrides: [babel, typescript],
+  rules: {},
+};
